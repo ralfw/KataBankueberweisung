41bb3ebbead0560189ff88c6fda79e57ab26c581
Matthias Bohlen <mbohlen@mbohlen.de>
2012-10-07 09:22:04 +0200

    Hauptprogramm angelegt, Compileroptionen eingestellt, so dass es läuft.

---
+++ mbohlen/KataBankUeberweisungCmdLine/src/main/groovy/de/mbohlen/katabankueberweisung/cmdline/Program.groovy
@@ -0,0 +1,13 @@
+package de.mbohlen.katabankueberweisung.cmdline
+
+/**
+ * @author Matthias Bohlen
+ *
+ */
+public class Program {
+
+	public static void main(String[] args) {
+		println "Willkommen bei der Kata Bank!"
+	}
+
+}
