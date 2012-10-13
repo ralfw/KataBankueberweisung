639e0b2997dccdd1e27c23b11d55ceb544d2ba5f
Ralf Westphal <info@ralfw.de>
2012-10-13 22:01:12 +0200

    Walking skeleton implementiert
    Der Flow für die Interaktionen steht auf oberster Abstraktionsebene.
    Die Anwendung läuft vom Start bis zum Ende durch alle Interaktionen -
    allerdings ohne Benutzereingaben. Die Interaktionen sind als Dummys
    implementiert.

--- ralfw/bankueberweisung.console/bankueberweisung.console/Main.cs
+++ ralfw/bankueberweisung.console/bankueberweisung.console/Main.cs
@@ -1,6 +1,9 @@
 using System;
 using npantarhei.runtime;
 using npantarhei.runtime.operations;
+using npantarhei.runtime.config;
+using System.Reflection;
+using npantarhei.runtime.contract;
 
 namespace bankueberweisung.console
 {
@@ -8,7 +11,51 @@ namespace bankueberweisung.console
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			var config = new FlowRuntimeConfiguration()
+								.AddStreamsFrom("bankueberweisung.console.root.flow", Assembly.GetExecutingAssembly())
+								.AddOperations(new AssemblyCrawler(Assembly.GetExecutingAssembly()));
+			using(var fr = new FlowRuntime(config)) {
+				fr.Process(".run");
+				fr.WaitForResult();
+			}
+		}
+	}
+
+
+	[StaticOperations]
+	class DummyOperations {
+		public static Tuple<string,string> Programmstart() {
+			return new Tuple<string,string>("20050550", "Hamburger Sparkasse");
+		}
+
+		public static void Phase1_Sender_pruefen(Tuple<string,string> sender_name_kontonummer) {
+			Log("Sender geprüft!");
+		}
+
+		public static void Phase2_Bewegung_pruefen(Tuple<double,string> betrag_text) {
+			Log("Bewegung geprüft!");
+		}
+
+		public static string Phase3_Empfaenger_pruefen(Tuple<string,string,string> empfänger_name_ktonr_blz) {
+			Log("Empfänger geprüft!");
+			return "30060660";
+		}
+
+		public static void Phase4_TAN_versenden(bool geprüft) {
+			Log("TAN versandt!");
+		}
+
+		public static void Phase5_Ueberweisung_durchfuehren(string TAN) {
+			Log("TAN geprüft und Überweisung ausgeführt!");
+		}
+
+		private static void Log(string message) {
+			var fgc = Console.ForegroundColor;
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			
+			Console.WriteLine("<<{0}>>", message);
+			
+			Console.ForegroundColor = fgc;
 		}
 	}
 }
--- ralfw/bankueberweisung.console/bankueberweisung.console/umgebung/portale/UI.cs
+++ ralfw/bankueberweisung.console/bankueberweisung.console/umgebung/portale/UI.cs
@@ -1,10 +1,54 @@
 using System;
+using npantarhei.runtime.contract;
 
 namespace bankueberweisung.console.umgebung.portale
 {
+	[EventBasedComponent]
 	public class UI
 	{
+		public void Start(Tuple<string,string> sender_blz_bankname) {
+			Console.WriteLine("Willkommen bei der Kata Bank ({0}, {1})", sender_blz_bankname.Item1, sender_blz_bankname.Item2);
+			Console.WriteLine("Ihr Name: ");
+			Console.WriteLine("Ihre Kontonummer: ");
+			Sender_erfasst(new Tuple<string,string>("Sendername", "Senderktonr"));
+		}
 
+		public void Bewegung_erfragen() {
+			Console.WriteLine("Überweisungsbetrag: ");
+			Console.WriteLine("Überweisungstext: ");
+			Bewegung_erfasst(new Tuple<double,string>(42.24, "Text"));
+		}
+
+		public void Empfaenger_erfragen() {
+			Console.WriteLine("Angaben zum Empfänger");
+			Console.WriteLine("Name: ");
+			Console.WriteLine("Kontonummer: ");
+			Console.WriteLine("BLZ: ");
+			Empfaenger_erfasst(new Tuple<string,string,string>("Empfängername", "Empfängerktonr", "EmpfängerBLZ"));
+		}
+
+		public void Bestaetigung_erbitten(string empfaenger_blz) {
+			Console.WriteLine("Bitte überprüfen Sie die Angaben.");
+			Console.WriteLine("Überweisung durchführen? [jN]");
+			Bestaetigt(true);
+		}
+
+		public void TAN_erfragen() {
+			Console.WriteLine("Bitte TAN eingeben: ");
+			TAN_erfasst("TAN1234");
+		}
+
+		public void Beenden() {
+			Console.WriteLine("Überweisung durchgeführt.");
+			Ende();
+		}
+
+		public event Action<Tuple<string,string>> Sender_erfasst;
+		public event Action<Tuple<double,string>> Bewegung_erfasst;
+		public event Action<Tuple<string,string,string>> Empfaenger_erfasst;
+		public event Action<bool> Bestaetigt;
+		public event Action<string> TAN_erfasst;
+		public event Action Ende;
 	}
 }
 
